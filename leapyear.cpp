4712219909d8a74011dcffa49c00c87138f5606e
Tanjilul Anwar <36304879+TanjilulAnwar@users.noreply.github.com>
2021-01-28 11:12:51 +0600

    Create leapyear.cpp

---
+++ leapyear.cpp
@@ -0,0 +1,22 @@
+
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+int year;
+  cin >> year;
+if(year % 4 == 0 &&  year % 100 !=0){
+   cout << "leap year";
+}
+else if(year % 400 == 0 ){
+ cout << "leap year";
+}
+else {
+  cout << "not leap year";
+}
+
+return 0;
+
+}
